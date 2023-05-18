372ed95c488dc652f69810e4b3da86fd01d4b9c2
Mohammed Saed <mohd.a.saed@gmail.com>
2023-05-18 16:58:49 +0300

    Finish signle post ui

--- App.styles.ts
+++ App.styles.ts
@@ -1,18 +1,148 @@
 import styled from 'styled-components/native';
-import Colors from './src/theme/Colors';
+import Entypo from 'react-native-vector-icons/Entypo';
 import { Sizes, Weights } from './src/theme/FontSizes';
+import Colors from './src/theme/Colors';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
 
 export const Container = styled.View`
 	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
+	flex-direction: column
+	justify-content: flex-start;
+	align-items: flex-start;
 	width: 100%;
-	height: 100%;
 `;
 
-export const Text = styled.Text`
-	color: ${Colors.primary};
+export const HeaderContainer = styled.View`
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-start;
+	align-items: center;
+	width: 100%;
+	padding: 10px;
+`;
+
+export const HeaderAvatar = styled.Image`
+	width: 50px;
+	height: 50px;
+	border-radius: 25px;
+	margin-right: 10px;
+`;
+
+export const HeaderUsername = styled.Text`
+	font-size: ${Sizes.medium};
+	font-weight: ${Weights.bold};
+	color: ${Colors.black};
+`;
+
+export const HeaderIcon = styled(Entypo)`
+	margin-left: auto;
+	font-size: ${Sizes.medium};
+`;
+
+export const PostImage = styled.Image`
+	width: 100%;
+	aspect-ratio: 1;
+`;
+
+export const FooterContainer = styled.View`
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: flex-start;
+	padding: 10px;
+`;
+
+export const FooterIconContainer = styled.View`
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-start;
+	align-items: center;
+	width: 100%;
+	margin-bottom: 5px;
+`;
+
+export const LikeIcon = styled(AntDesign)`
+	font-size: ${Sizes.extraLarge};
+	margin-right: 10px;
+	color: ${Colors.black};
+`;
+
+export const ChatIcon = styled(Ionicons)`
+	font-size: ${Sizes.extraLarge};
+	margin-right: 10px;
+	color: ${Colors.black};
+`;
+
+export const SendIcon = styled(Feather)`
+	font-size: ${Sizes.extraLarge};
+	color: ${Colors.black};
+`;
+
+export const BookmarkIcon = styled(Feather)`
+	font-size: ${Sizes.extraLarge};
+	margin-left: auto;
+	color: ${Colors.black};
+`;
+
+export const LikedByText = styled.Text`
+	color: ${Colors.black};
 	font-size: ${Sizes.default};
 	font-weight: ${Weights.default};
+	line-height: 18px;
+`;
+
+export const BoldText = styled.Text`
+	color: ${Colors.black};
+	font-size: ${Sizes.default};
+	font-weight: ${Weights.bold};
+	line-height: 18px;
+`;
+
+export const PostDescription = styled.Text`
+	color: ${Colors.black};
+	font-size: ${Sizes.default};
+	font-weight: ${Weights.default};
+	line-height: 18px;
+`;
+
+export const UsernameText = styled.Text`
+	color: ${Colors.black};
+	font-size: ${Sizes.default};
+	font-weight: ${Weights.bold};
+	line-height: 18px;
+`;
+
+export const ViewCommentsText = styled.Text`
+	width: 100%;
+	line-height: 18px;
+`;
+
+export const CommentsContainer = styled.View`
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-start;
+	align-items: center;
+	width: 100%;
+`;
+
+export const Comment = styled.Text`
+	color: ${Colors.black};
+	font-size: ${Sizes.default};
+	font-weight: ${Weights.default};
+	line-height: 18px;
+	flex: 1;
+`;
+
+export const CommentLikeIcon = styled(AntDesign)`
+	font-size: ${Sizes.small};
+	margin-right: 10px;
+	color: ${Colors.black};
+	margin: 0 10px;
+`;
+
+export const PublishDateText = styled.Text`
+	width: 100%;
+	line-height: 18px;
 `;
--- App.tsx
+++ App.tsx
@@ -1,13 +1,87 @@
 import React from 'react';
-import { Container, Text } from './App.styles';
-import AntDesign from 'react-native-vector-icons/AntDesign';
+import {
+	Container,
+	HeaderContainer,
+	HeaderAvatar,
+	HeaderUsername,
+	HeaderIcon,
+	PostImage,
+	FooterContainer,
+	FooterIconContainer,
+	LikeIcon,
+	ChatIcon,
+	SendIcon,
+	BookmarkIcon,
+	LikedByText,
+	BoldText,
+	PostDescription,
+	UsernameText,
+	ViewCommentsText,
+	CommentsContainer,
+	Comment,
+	CommentLikeIcon,
+	PublishDateText,
+} from './App.styles';
+import { SafeAreaView, ScrollView } from 'react-native';
 
 function App() {
 	return (
-		<Container>
-			<Text>Hello, World!</Text>
-			<AntDesign name="stepforward" size={15} />
-		</Container>
+		<SafeAreaView>
+			<Container>
+				<ScrollView>
+					<HeaderContainer>
+						<HeaderAvatar
+							source={{
+								uri: 'https://i.pinimg.com/originals/9c/e2/03/9ce2033132083598eee5c59e58571f82.jpg',
+							}}
+						/>
+						<HeaderUsername>moeabdol</HeaderUsername>
+						<HeaderIcon name="dots-three-horizontal" />
+					</HeaderContainer>
+
+					<PostImage
+						source={{
+							uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+						}}
+					/>
+
+					<FooterContainer>
+						<FooterIconContainer>
+							<LikeIcon name="hearto" />
+							<ChatIcon name="chatbubble-outline" />
+							<SendIcon name="send" />
+							<BookmarkIcon name="bookmark" />
+						</FooterIconContainer>
+
+						<LikedByText>
+							Like by <BoldText>moeabdol</BoldText> and{' '}
+							<BoldText>99 others</BoldText>
+						</LikedByText>
+
+						<PostDescription>
+							<UsernameText>moeabdol</UsernameText> Lorem ipsum dolor sit amet,
+							consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+							invidunt ut labore et dolore magna aliquyam erat, sed diam
+							voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+							Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+							dolor sit amet.
+						</PostDescription>
+
+						<ViewCommentsText>View all 16 comments</ViewCommentsText>
+
+						<CommentsContainer>
+							<Comment>
+								<UsernameText>moeabdol</UsernameText> Lorem ipsum dolor sit
+								amet, nonumy eirmod tempor Lorem ipsum dolor sit amet.
+							</Comment>
+							<CommentLikeIcon name="hearto" size={10} />
+						</CommentsContainer>
+
+						<PublishDateText>18 May, 2023</PublishDateText>
+					</FooterContainer>
+				</ScrollView>
+			</Container>
+		</SafeAreaView>
 	);
 }
 
