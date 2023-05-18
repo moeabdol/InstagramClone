845b901e448b72a039e6e4d7a4715051258ca71a
Mohammed Saed <mohd.a.saed@gmail.com>
2023-05-18 10:49:44 +0300

    Install styled-components

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Container } from './App.styles';
+import { Text } from 'react-native';
 
 function App() {
 	return (
-		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-			<Text>Helloooooo</Text>
-		</View>
+		<Container>
+			<Text>Hello, World!</Text>
+		</Container>
 	);
 }
 
