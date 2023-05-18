3b547581474638398366cba3b61e23ed3a2ae494
Mohammed Saed <mohd.a.saed@gmail.com>
2023-05-18 11:47:26 +0300

    Install react-native-vector-icons package

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container, Text } from './App.styles';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 function App() {
 	return (
 		<Container>
 			<Text>Hello, World!</Text>
+			<AntDesign name="stepforward" size={15} />
 		</Container>
 	);
 }
